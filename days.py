a8300c1557aa3d44997b5846b377d025dd27226e
rhiya-gandhi <rhiyagandhi99@gmail.com>
2026-09-11 15:42:49 +0530

    new day to learn python

---
+++ days.py
@@ -0,0 +1,17 @@
+day=input("enter a day")
+if (day=="1"):
+    print("today is monday")
+elif (day=="2"):
+    print("today is tuesday")
+elif (day=="3"):
+    print("today is wednesday")
+elif (day=="4"):
+    print("today is thursday")
+elif (day=="5"):
+    print("today is friday")
+elif (day=="6"):
+    print("today is saturday")
+elif (day=="7"):
+    print("today is sunday")
+else:
+    print("invalid day")
--- learnday2.py
+++ learnday2.py
@@ -1,5 +1,11 @@
-age = int(input("enter age"))
-if (age>18):
-    print("he is eligible to drive")
-else:
-    print("he is not eligible to drive")
+# age = int(input("enter age"))
+# if (age>18):
+#     print("he is eligible to drive")
+# else:
+#     print("he is not eligible to drive")
+
+# num = int(input("enter a number"))
+# if (num%2==0):
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd")
