fb99fbe3ff5866b7da1e906d98907e6ffedf156a
rhiya-gandhi <rhiyagandhi99@gmail.com>
2026-09-11 15:08:26 +0530

    new day to learn python

--- demo.py
+++ demo.py
@@ -2,11 +2,13 @@
 # print("number is odd:", a%2!=0)
 
 # age = int(input("enter age "))
-
 # print(f"{age} years = {age*365} days")
 
 # min = int(input("enter minutes"))
 # print(f"{min} is {min/60} hours {min%60} minutes")
 
-num = int(input("enter a num"))
-print(f"{num} : last digit is {num%10}")
+# num = int(input("enter a num"))
+# # print(f"{num} : last digit is {num%10}")
+# print(f"{num} : last digit is: {str(num)[-1]}")
+
+# age = int(input("enter age "))
---
+++ learnday2.py
@@ -0,0 +1,5 @@
+age = int(input("enter age"))
+if (age>18):
+    print("he is eligible to drive")
+else:
+    print("he is not eligible to drive")
